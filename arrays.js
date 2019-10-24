3cfea5ee170f8a2e1a847a656a89073d9130aebd
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-24 12:11:38 -0700

    Initial commit

---
+++ arrays.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+/** 1. printIndices */
+const printIndices = (items) => {
+	// Replace this with your code
+};
+
+
+/** 2. everyOtherItem */
+const everyOtherItem = (items) => {
+	// Replace this with your code
+};
+
+
+/** 3. smallestNItems */
+const smallestNItems = (items, n) => {
+	// Replace this with your code
+};
