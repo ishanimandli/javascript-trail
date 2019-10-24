01475023eb4d77d871c1fa8b5a9a58b41f7e7d9a
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-24 12:59:51 -0700

    finished isHometown and getFullName function in functions.js

--- functions.js
+++ functions.js
@@ -4,13 +4,20 @@
 /** 1. isHometown */
 
 // Define your function here
+function isHometown(town){
+
+  return (town === 'San Francisco');
+}
+//console.log(isHometown('San Franc'));
 
 
-** 2. getFullName */
+/** 2. getFullName */
 
 // Define your function here
-
-
+function getFullName(first_name, last_name){
+  return `${first_name} ${last_name}`;
+}
+console.log(getFullName('laura', 'miller'))
 /** 3. calculateTotal */
 
 // Define your function here
