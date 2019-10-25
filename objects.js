2d3d42f01e4cb5753e8fd5605f09fc7a2f3520ac
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-24 17:09:59 -0700

    Finished objects.js

--- objects.js
+++ objects.js
@@ -4,10 +4,33 @@
 /** 1. countWords */
 function countWords(phrase) {
 	// Replace this with your code
+  const wordCount = {};
+  for(const word of phrase.split(' ')){
+    if(Object.keys(wordCount).includes(word)){
+        wordCount[word] += 1;
+    }
+    else{
+      wordCount[word] = 1;
+    }
+  }
+
+  return wordCount;
 }
 
-
+console.log(countWords('hollo world hello hello'))
 /** 2. getMelonsAtPrice */
 function getMelonsAtPrice(price) {
 	// Replace this with your code
+  const melon_price = {
+        2.50: ['Cantaloupe', 'Honeydew'],
+        2.95: ['Watermelon'],
+        3.25: ['Musk', 'Crenshaw'],
+        14.25: ['Christmas']
+  };
+  //console.log(Object.keys(melon_price).includes(price.toString()));
+  if (Object.keys(melon_price).includes(price.toString())){
+      return melon_price[price];
+    }
 }
+
+console.log(getMelonsAtPrice(3.25));
