d99949a5db9c7e018b83744251aa3dcf4134b22a
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-24 18:01:32 -0700

    Further study one function completed

--- objects.js
+++ objects.js
@@ -34,3 +34,50 @@ function getMelonsAtPrice(price) {
 }
 
 console.log(getMelonsAtPrice(3.25));
+
+
+function wordsInCommon(words1, words2){
+  const words1_set = new Set(words1);
+  const words2_set = new Set(words2);
+
+  const result = new Set();
+
+  for (const word of words1_set){
+    if (words2_set.has(word)){
+      result.add(word);
+    }
+  }
+  return Array.from(result);
+}
+
+console.log(wordsInCommon(['hi','hi','set'],['hi','hi','setting']))
+
+function kidsGame(wordList){
+  const result = [].push(wordList.shift());
+  const wordListObj = {};
+
+  for(const word of wordList){
+    if(wordListObj[word[0]] === undefined){
+       wordListObj[word[0]] = [];
+      }
+    
+    wordListObj[word[0]].push(word);
+  }
+  let lengthOfResult = 0;
+  let startLetter;
+  let lengthOfWord;
+  let searchWord;
+  while(true){
+    lengthOfResult = result.length;
+    searchWord =result[lengthOfResult-1];
+    lengthOfWord = searchWord.length;
+  
+    startLetter = result[lengthOfResult-1][lengthOfWord-1];
+    if(wordListObj[startLetter] === []){
+      break;
+    }
+    result.push(wordListObj[startLetter].shift());
+  }
+  console.log(result)
+}
+kidsGame(["noon", "naan", "nun"]);
